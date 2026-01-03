cfec0fc3875357cbeed8aabdc15a622730e0f5f7
dengidengi <dengianassi@gmail.com>
2026-01-03 15:21:39 +0100

    auth setup

--- src/components/layout/UserButton.tsx
+++ src/components/layout/UserButton.tsx
@@ -4,12 +4,25 @@ import { useState, useEffect } from "react";
 import { CircleUser } from "lucide-react";
 
 export default function UserButton() {
+    const [showMenu, setShowMenu] = useState(false);
 
     return (
         <>
-        <button>
+        <div className="relative">
+        <button onClick={() => setShowMenu(!showMenu)} className="cursor-pointer">
             <CircleUser size={32} />
-          </button>
+        </button>
+        
+        {showMenu && (
+            <div className="absolute right-4 mt-2 w-48 bg-white border-none rounded-md shadow-lg z-10">
+                <ul>
+                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Profile</li>
+                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Settings</li>
+                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Logout</li>
+                </ul>
+            </div>
+        )}
+        </div>
         </>
     );
 }
--- src/components/layout/UserListItem.tsx
+++ src/components/layout/UserListItem.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Link from "next/link";
 import { User } from "@/types/types";
+
 
 export default function UserListItem({user}:{user:User}) {
     return (
         <>
-        <li>{user.name}</li>
+        <li className="cursor-pointer hover:bg-gray-100">
+            <Link href={`/profile/${user.id}`}>
+                <div className="flex items-center gap-2">
+                    <p>I</p>
+                    <p>{user.name}</p>
+                </div>
+            </Link>
+        </li>
         </>
     );
 }
